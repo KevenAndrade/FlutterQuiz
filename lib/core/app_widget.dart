852d6cad9c78da114dc97afc7258ca73598a6da7
Keven Andrade <hirlandeer7@gmail.com>
2021-04-19 14:38:42 -0100

    day 1 first

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutterquiz/home/home_page.dart';
+import 'package:flutterquiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Quiz",
+      home: Home(),
+    );
+  }
+}
