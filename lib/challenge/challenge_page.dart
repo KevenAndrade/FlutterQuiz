ef0a7eaf52ddab8c4a7449c7a7182f919230541b
Keven Andrade <hirlandeer7@gmail.com>
2021-04-22 15:39:29 -0100

    Awnser

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -12,16 +12,20 @@ class _ChalengePageState extends State<ChalengePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize:Size.fromHeight(60),
-          child: SafeArea(top:true, child: QuestionIndicator()),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: SafeArea(top: true, child: QuestionIndicator()),
         ),
-      body: Column(
-        children: [
-          Question(question:"Pa kz k ta sirve flutter?"),
-         
-        ],
-      )
-    );
+        body: Column(
+          children: [
+            Question(question: "Pa kz k ta sirve flutter?"),
+            Awnser(title: "Flutter", isRight: true, isSelected: true),
+            Awnser(title: "Flutter", isRight: false, isSelected: true),
+            Awnser(title: "Flutter", isRight: true, isSelected: false),
+            Awnser(title: "Flutter"),
+            Awnser(title: "Flutter"),
+            Awnser(title: "Flutter"),
+          ],
+        ));
   }
 }
